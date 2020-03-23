00baa8bd9effa1ebc77b7c4242dee1bd35b10e11
silverwing2003 <nsuguitan@gmail.com>
2020-03-22 22:34:11 -0700

    I made some tweaks

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+#
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
